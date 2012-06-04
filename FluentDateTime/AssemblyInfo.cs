6626472b38a1c41ae21a15e79f6c97be45c6511c
Eric Schultz <wwahammy@gmail.com>
2012-06-04 15:42:38 -0500

    Updated Version

--- FluentDateTime/AssemblyInfo.cs
+++ FluentDateTime/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("FluentDateTime")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ae721b00-f631-4986-a4ff-68643bfcba4a")]
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.6.0.1")]
+[assembly: AssemblyFileVersion("1.6.0.1")]
 
